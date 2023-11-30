f31b89098c6a33192e27a21e3a10fc06e4426e29
salbyul <kpast96@gmail.com>
2023-12-01 00:09:28 +0900

    refactor: ErrorResponse에 LocalDateTime 추가

--- src/main/java/com/example/libraryManagement/common/exception/GlobalException.java
+++ src/main/java/com/example/libraryManagement/common/exception/GlobalException.java
@@ -2,12 +2,20 @@ package com.example.libraryManagement.common.exception;
 
 import com.example.libraryManagement.common.response.error.ErrorType;
 
+import java.time.LocalDateTime;
+
 public class GlobalException extends RuntimeException {
 
     protected final ErrorType errorType;
+    private final LocalDateTime dateTime;
 
     public GlobalException(final ErrorType errorType) {
         super(errorType.getMessage());
         this.errorType = errorType;
+        this.dateTime = LocalDateTime.now();
+    }
+
+    public final LocalDateTime getDateTime() {
+        return this.dateTime;
     }
 }
--- src/main/java/com/example/libraryManagement/common/response/error/ErrorResponse.java
+++ src/main/java/com/example/libraryManagement/common/response/error/ErrorResponse.java
@@ -4,6 +4,8 @@ import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import java.time.LocalDateTime;
+
 @Getter
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
 public class ErrorResponse {
@@ -11,13 +13,14 @@ public class ErrorResponse {
     public static final String GLOBAL = "GLOBAL";
     private final String domain;
     private final String message;
+    private final LocalDateTime dateTime;
 
     public static ErrorResponse generateAboutGlobal(final ErrorType errorType) {
-        return new ErrorResponse(GLOBAL, errorType.getMessage());
+        return new ErrorResponse(GLOBAL, errorType.getMessage(), LocalDateTime.now());
     }
 
-    public static ErrorResponse generate(final String domain, final ErrorType errorType) {
-        return new ErrorResponse(domain, errorType.getMessage());
+    public static ErrorResponse generate(final String domain, final ErrorType errorType, final LocalDateTime dateTime) {
+        return new ErrorResponse(domain, errorType.getMessage(), dateTime);
     }
 
 }
